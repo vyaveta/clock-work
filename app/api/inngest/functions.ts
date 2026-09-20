ff6ee964f31009ce2e34f295a7f858d156f89cef
Adwaith <108970016+vyaveta@users.noreply.github.com>
2026-09-20 20:45:04 +0530

    done

--- app/api/inngest/functions.ts
+++ app/api/inngest/functions.ts
@@ -1,15 +1,40 @@
 import { inngest } from "@/inngest/client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai"
+import {createAnthropic} from "@ai-sdk/anthropic"
+import { generateText } from "ai";
+
+
+const google = createGoogleGenerativeAI()
+const openai = createOpenAI()
+const anthropic = createAnthropic()
 
 // pages/api/inngest.ts - add to existing file
 export const processTask = inngest.createFunction(
-  { id: "process-task", triggers: { event: "app/task.created" } },
+  { id: "execute-ai", triggers: { event: "execute/ai" } },
   async ({ event, step }) => {
-    const result = await step.run("handle-task", async () => {
-      return { processed: true, id: event.data.id };
-    });
+    
+    const {steps: geminiSteps} = await step.ai.wrap("gemini-generate-text", generateText, {
+      system: "You are a helpful assistant.",
+      prompt: "write a sweet greeting message for user. do not return anything else.",
+      model: google("gemini-3.6-flash")
+    })
+    const {steps: openaiSteps} = await step.ai.wrap("openai-generate-text", generateText, {
+      system: "You are a helpful assistant.",
+      prompt: "write a sweet greeting message for user. do not return anything else.",
+      model: openai("gpt-4.1")
+    })
+    const {steps: anthropicSteps} = await step.ai.wrap("anthropic-generate-text", generateText, {
+      system: "You are a helpful assistant.",
+      prompt: "write a sweet greeting message for user. do not return anything else.",
+      model: anthropic("claude-sonnet-4-0")
+    })
 
-    await step.sleep("pause", "1s");
+    return {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps
+    }
 
-    return { message: `Task ${event.data.id} complete`, result };
   }
 );
